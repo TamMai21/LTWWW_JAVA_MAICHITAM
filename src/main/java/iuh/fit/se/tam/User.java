63be96c7418170af568006fb9d466573c86839cb
H82M19\Student <Student>
2025-08-24 14:55:00 +0700

    tuan1

---
+++ src/main/java/iuh/fit/se/tam/User.java
@@ -0,0 +1,15 @@
+package iuh.fit.se.tam;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    String name;
+    int age;
+    String address;
+
+}
